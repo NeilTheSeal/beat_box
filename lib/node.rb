2e7a39b3bf249d8d190825755e32b05a6c035379
NeilTheSeal <neil.hendren@gmail.com>
2024-02-07 15:32:33 -0700

    Add more specs

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,15 @@
 # Documentation for Node
 class Node
+  attr_accessor :data,
+                :next,
+                :previous
+
   def initialize(args)
     @data = {}
-    args.keys.each { |key|
-      @data[key] = args[key]
-    }
+    args[:data].each_key do |key|
+      @data[key] = args[:data][key]
+    end
+    @next = args[:next] || nil
+    @previous = args[:previous] || nil
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,11 +3,15 @@
 require "rspec"
 require "./lib/node"
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Node do
   before(:each) do
-    @node = Node.new({
+    @data = {
       name: "Neil Hendren",
       age: 30
+    }
+    @node = Node.new({
+      data: @data
     })
   end
 
@@ -15,5 +19,25 @@ RSpec.describe Node do
     expect(@node).to be_an_instance_of(Node)
   end
 
-  it ""
+  it "has a data method" do
+    expect(@node.data).to eq(@data)
+  end
+
+  it "initializes with default of nil value for previous and next" do
+    expect(@node.next).to eq(nil)
+    expect(@node.previous).to eq(nil)
+  end
+
+  it "initializes with previous value of a node when previous is specified" do
+    new_node = Node.new({
+      previous: @node,
+      data: {
+        name: "John",
+        age: 63
+      }
+    })
+    expect(new_node.previous).to eq(@node)
+  end
 end
+# rubocop:enable Metrics/BlockLength
+#
